9551a5432927ef8dabd8ae0b8d59f57ec51cd3a9
Ethan Tabachneck <ethan.tabachneck@stonybrook.edu>
2021-09-25 12:40:27 -0400

    this is what I am working with. Should just be a video. some other stuff being worked on in there

--- video.cpp
+++ video.cpp
@@ -1,27 +1,43 @@
 #include <opencv2/core.hpp>
+#include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 using namespace cv;
 using namespace std;
 int main(int, char**)
 {
-    Mat frame;
+    Mat frame, gray;
+    Mat res, loc;
+    double thres = 0.84;
+
     //--- INITIALIZE VIDEOCAPTURE
     VideoCapture cap;
-    // open the default camera using default API
-    // cap.open(2);
-    // OR advance usage: select any API backend
     int deviceID = 2;             // 0 = open default camera
     int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-    // open selected camera using selected API
     cap.open(deviceID, apiID);
     // check if we succeeded
     if (!cap.isOpened()) {
         cerr << "ERROR! Unable to open camera\n";
         return -1;
     }
+    //Open template image
+    Mat img;
+    img = imread("../data/grapefruit.png", IMREAD_GRAYSCALE);
+    //I make from a build directory, so you might need to remove '../'
+    // success?
+    if (!img.data) {
+	cerr << "ERROR! Unable to open file\n";
+	return -2;
+    }
+    //imshow("Template", img);
+    Size sz = img.size();
+    int w = sz.width;
+    int h = sz.height;
+
     //--- GRAB AND WRITE LOOP
     cout << "Start grabbing" << endl
         << "Press any key to terminate" << endl;
@@ -34,6 +50,17 @@ int main(int, char**)
             cerr << "ERROR! blank frame grabbed\n";
             break;
         }
+	//Operate on image
+	cvtColor(frame, gray, COLOR_BGR2GRAY);
+	//matchTemplate(gray, img, res, TM_CCOEFF_NORMED);
+	//for (x=0,x++,x<res.size.width)
+	//{
+	//	for (y=0,y++,y<res.size.height)
+	//	{
+	//		if (res[x,y] >= thres)
+	//			rectangle
+	//	}
+	//}
         // show live and wait for a key with timeout long enough to show images
         imshow("Live", frame);
         if (waitKey(5) >= 0)
